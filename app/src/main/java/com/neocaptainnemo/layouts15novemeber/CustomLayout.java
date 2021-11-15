ef9023ec971937ebd85b8c7a4bd65ad90364303c
Vasilii Ponteleev <neocaptainnemo@gmail.com>
2021-11-15 21:54:27 +0300

    Initial commit

---
+++ app/src/main/java/com/neocaptainnemo/layouts15novemeber/CustomLayout.java
@@ -0,0 +1,29 @@
+package com.neocaptainnemo.layouts15novemeber;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.ViewGroup;
+
+public class CustomLayout extends ViewGroup {
+
+    public CustomLayout(Context context) {
+        super(context);
+    }
+
+    public CustomLayout(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public CustomLayout(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+
+    public CustomLayout(Context context, AttributeSet attrs, int defStyleAttr, int defStyleRes) {
+        super(context, attrs, defStyleAttr, defStyleRes);
+    }
+
+    @Override
+    protected void onLayout(boolean changed, int l, int t, int r, int b) {
+
+    }
+}
